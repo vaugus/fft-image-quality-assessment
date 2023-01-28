65db85e368665691a06946f570f01137bed343a0
Victor Augusto <vaugusto@alumni.usp.br>
2023-01-27 21:21:31 -0300

    feat: save_image function and unit test

--- test/image_io_test.cpp
+++ test/image_io_test.cpp
@@ -5,9 +5,9 @@
 #include "../include/test_utils.hpp"
 
 TestUtils *utils = new TestUtils();
+ImageIO *io = new ImageIO();
 
 TEST(open_image, should_return_cv_mat_with_pixels) {
-    ImageIO *io = new ImageIO();
     cv::Mat image = io->open_image("test/resources/atom.png");
 
     EXPECT_EQ(0, cv::norm(image, utils->create_atom_image(), cv::NORM_L1));
