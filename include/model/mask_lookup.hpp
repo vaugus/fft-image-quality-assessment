8286ba7f75a3c91f0e5dc26905be72a37ae3618b
Victor Augusto <vaugusto@alumni.usp.br>
2023-02-19 16:26:43 -0300

    feature: singleton to cache binary masks

---
+++ include/model/mask_lookup.hpp
@@ -0,0 +1,29 @@
+#ifndef MASK_LOOKUP_HPP
+#define MASK_LOOKUP_HPP
+
+#include <map>
+#include <mutex>
+#include "opencv4/opencv2/core.hpp"
+
+class MaskLookup {
+  private:
+    static MaskLookup *instance;
+    static std::mutex mutex;
+    
+
+    MaskLookup();
+    ~MaskLookup();
+  public:
+
+    std::map<std::pair<int, int>, cv::Mat> index;
+
+    MaskLookup(MaskLookup &other) = delete;
+    void operator=(const MaskLookup &) = delete;
+    static MaskLookup *get_instance();
+
+    bool mask_exists(int width, int height);
+    cv::Mat of(int width, int height);
+    void insert(cv::Mat mask);
+};
+
+#endif
