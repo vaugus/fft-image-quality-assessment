f0085d71b8596c1582947a15f6b975c0de8490d2
Victor Augusto <vaugusto@alumni.usp.br>
2023-02-20 20:46:20 -0300

    feature: pad image with zeros

--- include/wrappers/draw.hpp
+++ include/wrappers/draw.hpp
@@ -6,18 +6,35 @@
 #include "opencv4/opencv2/imgcodecs.hpp"
 #include "opencv4/opencv2/highgui.hpp"
 
+struct Border {
+  int top;
+  int bottom;
+  int left;
+  int right;
+  int type;
+  cv::Scalar value;
+};
+
 class Draw {
   private:
-    const cv::Scalar WHITE = cv::Scalar(255, 255, 255);
     void draw_line(cv::Mat image, cv::Point center, cv::Point head, cv::Scalar color, int line_type, int shift);
 
   public:
+    static const cv::Scalar ZERO_BGR;
+    static const cv::Scalar ONE_BGR;
+
+    cv::Mat apply_border(cv::Mat source, cv::Mat target, Border border);
+    Border create_zeros_square_border(cv::Mat image);
+
     cv::Mat zeros(int width, int height, int type);
+    cv::Mat ones(int width, int height, int type);
     void white_line(cv::Mat image, cv::Point center, cv::Point head);
 
     void draw_elipse(cv::Mat image, cv::Point center, double angle);
     void draw_filled_circle(cv::Mat image, cv::Point center);
     cv::Mat create_atom_image();
+
+    cv::Mat pad_with_zeros(cv::Mat const &image);
 };
 
 #endif
