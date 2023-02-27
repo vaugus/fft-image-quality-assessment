1b62583f659ad7395c582b0ec12cbd618bd8a718
Victor Augusto <vaugusto@alumni.usp.br>
2023-02-27 19:54:30 -0300

    feature: spectrum analysis as a builder to pipe the fft processing

--- src/model/spectrum_analysis.cpp
+++ src/model/spectrum_analysis.cpp
@@ -1,5 +1,4 @@
 #include "../../include/model/spectrum_analysis.hpp"
-#include <iostream>
 
 extern Draw *draw;
 extern ImageIO *io;
@@ -7,31 +6,48 @@ FourierTransform *fft = new FourierTransform();
 extern MaskLookup *lookup;
 
 SpectrumAnalysis& SpectrumAnalysis::prepare_image(cv::Mat image) {
-  gray = draw->pad_with_zeros(io->grayscale(image));
+  this->gray = draw->pad_with_zeros(io->grayscale(image));
   return *this;
 }
 
 SpectrumAnalysis& SpectrumAnalysis::apply_fft() {
-  complex_image = fft->perform_fft(gray);
-  spectrum = fft->shift_quadrants(
-              fft->logarithmic_scale(
-                fft->compute_magnitude(complex_image)));
-
+  this->complex_image = fft->perform_fft(gray);
+  this->spectrum = fft->shift_quadrants(fft->compute_magnitude(complex_image));
   return *this;
 }
 
 SpectrumAnalysis& SpectrumAnalysis::apply_mask() {
   cv::Mat mask = lookup->of(spectrum.rows, spectrum.cols);
   mask.convertTo(mask, spectrum.type(), 1.0 / 255.0);
-  spectrum = spectrum.mul(mask);
+  this->spectrum = spectrum.mul(mask);
 
   return *this;
 }
 
 SpectrumAnalysis& SpectrumAnalysis::compute_maximum_absolute_value() {
-  spectrum_abs_values = cv::abs(spectrum);
+  this->absolute_spectrum = cv::abs(this->spectrum);
 
   double min; 
-  cv::minMaxLoc(spectrum_abs_values, &min, &this->max);
+  cv::minMaxLoc(this->absolute_spectrum, &min, &this->max);
   return *this;
+}
+
+ComplexImage SpectrumAnalysis::get_complex_image() {
+  return this->complex_image;
+}
+
+cv::Mat SpectrumAnalysis::get_preprocessed_image() {
+  return this->gray;
+}
+
+cv::Mat SpectrumAnalysis::get_spectrum() {
+  return this->spectrum;
+}
+
+cv::Mat SpectrumAnalysis::get_absolute_spectrum() {
+  return this->absolute_spectrum;
+}
+
+double SpectrumAnalysis::get_max_absolute_value() {
+  return this->max;
 }
