b55e29f9ca1312589cead11a88c280805c42c685
Victor Augusto <vaugusto@alumni.usp.br>
2023-12-10 22:27:59 -0300

    gardening

--- include/model/builder.hpp
+++ include/model/builder.hpp
@@ -4,8 +4,8 @@
 template <typename T>
 class Builder {
   public:
-  static T builder() { return {}; }
-  T & build() { return static_cast<T&>(*this); }
+    static T builder() { return {}; }
+    T & build() { return static_cast<T&>(*this); }
 };
 
 #endif
